b8179324a6cf1f010d0cc755c920b82c4ed7150f
Kurtis Houser <houserkurtis@gmail.com>
2019-12-05 22:44:53 -0800

    Combine device data into one object with one socket.io event listener

--- src/App.js
+++ src/App.js
@@ -27,15 +27,11 @@ const initialLocation = {
 
 function App() {
   const [socketId, setSocketId] = useState(null);
-  const [location, setLocation] = useState(initialLocation);
-  const [activeTrip, setActiveTrip] = useState(null);
-  const [completedTrips, setCompletedTrips] = useState([]);
-
-  const center = {
-    lat: location.geometry.coordinates[1],
-    lng: location.geometry.coordinates[0]
-  };
-  const zoom = 14;
+  const [deviceDetails, setDeviceDetails] = useState({
+    currentLocation: initialLocation,
+    currentTrip: null,
+    completedTrips: [],
+  });
 
   useEffect(() => {
     socket.once('connect', () => setSocketId(socket.id));
@@ -46,19 +42,24 @@ function App() {
   }, [socketId]);
 
   useEffect(() => {
-    socket.once('location-updated', newLocation => setLocation(newLocation));
-  }, [location]);
-
-  useEffect(() => {
-    socket.once('trip-active', activeTrip => setActiveTrip(activeTrip));
-  }, [activeTrip]);
-
-  useEffect(() => {
-    socket.once('trip-complete', newTrips => {
-      setActiveTrip(null);
-      setCompletedTrips(completedTrips.concat(newTrips));
+    socket.once('device-update', payload => {
+      setDeviceDetails({
+        currentLocation: payload.currentLocation,
+        currentTrip: payload.currentTrip,
+        completedTrips: deviceDetails.completedTrips.concat(
+          payload.completedTrips
+        )
+      });
     });
-  }, [completedTrips]);
+  }, [deviceDetails]);
+
+  const { currentLocation, currentTrip, completedTrips } = deviceDetails;
+
+  const center = {
+    lat: currentLocation.geometry.coordinates[1],
+    lng: currentLocation.geometry.coordinates[0]
+  };
+  const zoom = 14;
 
   return (
     <div>
@@ -80,12 +81,12 @@ function App() {
           <Marker position={center}>
             <Popup>
               <div>
-                <strong>Device:</strong> {location.properties.device_id}
+                <strong>Device:</strong> {currentLocation.properties.device_id}
               </div>
               <div>
                 <strong>Time:</strong>{' '}
-                {location.properties.timestamp &&
-                  new Date(location.properties.timestamp).toLocaleTimeString(
+                {currentLocation.properties.timestamp &&
+                  new Date(currentLocation.properties.timestamp).toLocaleTimeString(
                     'en-US'
                   )}
               </div>
@@ -95,100 +96,100 @@ function App() {
         </Map>
 
         <div className='content-section'>
-          <h2>Location Details</h2>
-          {location.properties.timestamp && (
+          <h2>Current Location</h2>
+          {currentLocation.properties.timestamp && (
             <div>
               <div>
                 <span className='field-label'>Device:</span>{' '}
-                {location.properties.device_id}
+                {currentLocation.properties.device_id}
               </div>
               <div>
                 <span className='field-label'>Battery:</span>{' '}
-                {location.properties.battery_level &&
-                  `${(location.properties.battery_level * 100).toFixed(1)} %`}
+                {currentLocation.properties.battery_level &&
+                  `${(currentLocation.properties.battery_level * 100).toFixed(1)} %`}
               </div>
               <div>
                 <span className='field-label'>Battery State:</span>{' '}
-                {location.properties.battery_state}
+                {currentLocation.properties.battery_state}
               </div>
               <div>
                 <span className='field-label'>WiFi:</span>{' '}
-                {location.properties.wifi}
+                {currentLocation.properties.wifi}
               </div>
               <div>
                 <span className='field-label'>Time:</span>{' '}
-                {location.properties.timestamp &&
-                  new Date(location.properties.timestamp).toLocaleTimeString(
+                {currentLocation.properties.timestamp &&
+                  new Date(currentLocation.properties.timestamp).toLocaleTimeString(
                     'en-US'
                   )}
               </div>
               <div>
                 <span className='field-label'>Speed:</span>{' '}
-                {location.properties.speed &&
-                  `${(location.properties.speed * 2.23694).toFixed(1)} mph (${
-                    location.properties.speed
+                {currentLocation.properties.speed &&
+                  `${(currentLocation.properties.speed * 2.23694).toFixed(1)} mph (${
+                    currentLocation.properties.speed
                   } m/s)`}
               </div>
               <div>
                 <span className='field-label'>Motion:</span>{' '}
-                {location.properties.motion &&
-                  location.properties.motion.join(', ')}
+                {currentLocation.properties.motion &&
+                  currentLocation.properties.motion.join(', ')}
               </div>
               <div>
                 <span className='field-label'>Altitude:</span>{' '}
-                {location.properties.altitude &&
-                  `${(location.properties.altitude * 3.28084).toFixed(1)} ft (${
-                    location.properties.altitude
+                {currentLocation.properties.altitude &&
+                  `${(currentLocation.properties.altitude * 3.28084).toFixed(1)} ft (${
+                    currentLocation.properties.altitude
                   } m)`}
               </div>
               <div>
                 <span className='field-label'>Vertical Accuracy:</span>{' '}
-                {location.properties.vertical_accuracy &&
-                  `${(location.properties.vertical_accuracy * 3.28084).toFixed(
+                {currentLocation.properties.vertical_accuracy &&
+                  `${(currentLocation.properties.vertical_accuracy * 3.28084).toFixed(
                     1
-                  )} ft (${location.properties.vertical_accuracy} m)`}
+                  )} ft (${currentLocation.properties.vertical_accuracy} m)`}
               </div>
               <div>
                 <span className='field-label'>Horizontal Accuracy:</span>{' '}
-                {location.properties.horizontal_accuracy &&
+                {currentLocation.properties.horizontal_accuracy &&
                   `${(
-                    location.properties.horizontal_accuracy * 3.28084
+                    currentLocation.properties.horizontal_accuracy * 3.28084
                   ).toFixed(1)} ft (${
-                    location.properties.horizontal_accuracy
+                    currentLocation.properties.horizontal_accuracy
                   } m)`}
               </div>
               <div>
                 <span className='field-label'>Latitude:</span>{' '}
-                {location.geometry.coordinates[1].toFixed(6)}
+                {currentLocation.geometry.coordinates[1].toFixed(6)}
               </div>
               <div>
                 <span className='field-label'>Longitude:</span>{' '}
-                {location.geometry.coordinates[0].toFixed(6)}
+                {currentLocation.geometry.coordinates[0].toFixed(6)}
               </div>
             </div>
           )}
         </div>
 
         <div className='content-section'>
-          <h2>Active Trip</h2>
-          {activeTrip && (
+          <h2>Current Trip</h2>
+          {currentTrip && (
             <div>
               <div>
                 <span className='field-label'>Trip Start Time:</span>{' '}
                 {new Date(
-                  activeTrip.start_location.properties.timestamp
+                  currentTrip.start_location.properties.timestamp
                 ).toLocaleTimeString('en-US')}
               </div>
               <div>
                 <span className='field-label'>Trip Current Time:</span>{' '}
                 {new Date(
-                  activeTrip.current_location.properties.timestamp
+                  currentTrip.current_location.properties.timestamp
                 ).toLocaleTimeString('en-US')}
               </div>
               <div>
                 <span className='field-label'>Trip Distance:</span>{' '}
-                {`${(activeTrip.distance / 1609.344).toFixed(2)} miles (${(
-                  activeTrip.distance / 1000
+                {`${(currentTrip.distance / 1609.344).toFixed(2)} miles (${(
+                  currentTrip.distance / 1000
                 ).toFixed(2)} km)`}
               </div>
             </div>
